a93fcbd45865690ef4303f697fd4a6e8c7474fb1
Reuben Daniel <reubendaniel41@gmail.com>
2024-12-12 11:23:54 -0600

    Drawn a circle to the center of the screen

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,16 @@
 
 int main() {
     auto window = sf::RenderWindow({1920u, 1080u}, "CMake SFML Project");
+    sf::Color color(80, 0, 0);
     window.setFramerateLimit(144);
 
     std::cout << "Hello world!!" << std::endl;
+
+    
+
+    sf::CircleShape circle(100);
+    circle.setPosition(((window.getSize().x / 2) - 100), ((window.getSize().y / 2) - 100));
+    circle.setFillColor(color);
 
     while (window.isOpen()) {
         for (auto event = sf::Event(); window.pollEvent(event);) {
@@ -13,8 +20,11 @@ int main() {
                 window.close();
             }
         }
-
+        // Clear, draw, display
         window.clear();
+
+        window.draw(circle);
+
         window.display();
     }
 }
