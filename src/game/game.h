75ab27fd0ce58ca427812daaaf1dea85813bed94
Reuben Daniel <reubendaniel41@gmail.com>
2024-12-15 23:14:30 -0600

    added a destructor to delete the player from the heap

--- src/game/game.h
+++ src/game/game.h
@@ -12,6 +12,9 @@ public:
     Game(sf::RenderWindow& window) {
         player = new Player(window);
     };
+    ~Game() {
+        delete player;
+    }
     void run(sf::RenderWindow& window);
     void handleInput(sf::RenderWindow& window, sf::Event event);
 };
